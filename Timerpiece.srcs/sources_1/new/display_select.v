e4a79bd85f4002035b1195a0071f64645c531645
younghyun0702 <younghyun0702@gmail.com>
2026-04-18 17:06:36 +0900

    fnd 컨트롤러 dot 위치 코드 추가 시뮬레이션은 안 했음

---
+++ Timerpiece.srcs/sources_1/new/display_select.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/04/18 17:05:38
+// Design Name: 
+// Module Name: display_select
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module display_select(
+
+    );
+endmodule
